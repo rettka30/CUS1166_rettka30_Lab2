032e4487f46e3d09f2834b1d51c460fcd5f02ba3
Andrew Rettko <andrew.rettko17@stjohns.edu>
2019-02-13 15:46:14 -0500

    Routes

--- playground/app.py
+++ playground/app.py
@@ -13,12 +13,7 @@ def welcome(student_name):
 @app.route('/roster/<int:grade_view>')
 def roster(grade_view):
     class_roster = [("John", 88, "Freshman"), ("Herald", 92, "Junior"), ("Ann", 100, "Senior"), ("Peter", 85, "Sophomore"), ("Mary", 95, "Junior"), ("Jerry", 76, "Freshman"), ("Justin", 81, "Senior")]
-    if (grade_view==1):
-        return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
-    elif (grade_view==2):
-        return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
-    else:
-        return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
+    return render_template("roster.html", class_roster=class_roster, grade_view=grade_view)
 
 
 if __name__ == "__main__":
